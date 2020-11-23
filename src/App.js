25e7bec54909522e608dde5a8f633c2166382fee
dkm113 <gkojima1221@gmail.com>
2020-11-23 15:24:59 +0900

    [update]: スタート画面のデザインを修正。パンダくんの追加。

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render () {
     return (
       <div>
-        <h1>人生を豊かにするお金にまつわる5つの力 診断テスト！</h1>
+        <h1 className="mainTitle">人生を豊かにするお金にまつわる5つの力 診断テスト！</h1>
         <Route path="/" component={StartApp} exact={true} />
         <Route path="/form" component={FormApp} />
         <Route path="/result" component={ResultApp} />
--- src/components/start.js
+++ src/components/start.js
@@ -13,9 +13,20 @@ class StartApp extends Component {
             <div>
                 <div className="container">
                     <div className="startVisual">
+                        <div className="flexArea">
+                            <div className="flex-1">
+                                <img width="200px" src="https://frame-illust.com/fi/wp-content/uploads/2017/03/9694-480x480.png" />
+                            </div>
+                            <div className="flex-4 comment">
+                                <p>
+                                    ここでは、リベラルアーツ大学で最も大切にしている「5つの力」について、
+                                    あなたの今現在の力を分析するよ。楽しんでいってね。
+                                </p>
+                            </div>
+                        </div>
                         <img width="600px" src="https://cdn.shortpixel.ai/client/to_avif,q_glossy,ret_img,w_960/https://liberaluni.com/wp-content/uploads/2018/04/191003itutunotikara1.png" />
                         <br />
-                        <button className="startBtn" onClick={this.startForm.bind(this)}>5つの力を診断-さあ、あなたの5つの力は？-</button>
+                        <button className="startBtn" onClick={this.startForm.bind(this)}>5つの力を診断!<br />- さあ、あなたの5つの力は？ -</button>
                     </div>
                 </div>
             </div>
