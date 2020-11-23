4609dc41ec534891b67b5f2dd04a02224fddab51
dkm113 <gkojima1221@gmail.com>
2020-11-23 16:34:05 +0900

    [update]: ラジオボタンにMaterial-UIを適用

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radio from '@material-ui/core/Radio';
 import '../index.css';
 import './form.css';
 
@@ -34,42 +35,42 @@ class FormApp extends Component {
                         <dl>
                             <dt>Q1:確定申告を利用していますか？</dt>
                             <dd>
-                                <input type="radio" name="q1" value="2" checked={this.state.q1 === "2"} onChange={()=>this.setState({q1: "2"})} />
+                                <Radio name="q1" value="2" checked={this.state.q1 === "2"} onChange={()=>this.setState({q1: "2"})} />
                                 <label>利用している</label>
                                 <br />
-                                <input type="radio" name="q1" value="0" checked={this.state.q1 === "0"} onChange={()=>this.setState({q1: "0"})} />
+                                <Radio name="q1" value="0" checked={this.state.q1 === "0"} onChange={()=>this.setState({q1: "0"})} />
                                 <label>利用していない</label>
                             </dd>
                             <dt>Q2:月々の保険料はいくらですか？</dt>
                             <dd>
-                                <input type="radio" name="q2" value="2" checked={this.state.q2 === "2"} onChange={()=>this.setState({q2: "2"})} />
+                                <Radio name="q2" value="2" checked={this.state.q2 === "2"} onChange={()=>this.setState({q2: "2"})} />
                                 <label>5000円以内</label>
                                 <br />
-                                <input type="radio" name="q2" value="0" checked={this.state.q2 === "0"} onChange={()=>this.setState({q2: "0"})} />
+                                <Radio name="q2" value="0" checked={this.state.q2 === "0"} onChange={()=>this.setState({q2: "0"})} />
                                 <label>5000円を超えている</label>
                             </dd>
                             <dt>Q3:複数の収入源を持っていますか？</dt>
                             <dd>
-                                <input type="radio" name="q3" value="2" checked={this.state.q3 === "2"} onChange={()=>this.setState({q3: "2"})} />
+                                <Radio name="q3" value="2" checked={this.state.q3 === "2"} onChange={()=>this.setState({q3: "2"})} />
                                 <label>2つ以上持っている</label>
                                 <br />
-                                <input type="radio" name="q3" value="0" checked={this.state.q3 === "0"} onChange={()=>this.setState({q3: "0"})} />
+                                <Radio name="q3" value="0" checked={this.state.q3 === "0"} onChange={()=>this.setState({q3: "0"})} />
                                 <label>1つの収入源しかない</label>
                             </dd>
                             <dt>Q4:私募ファンドに投資していますか？</dt>
                             <dd>
-                                <input type="radio" name="q4" value="2" checked={this.state.q4 === "2"} onChange={()=>this.setState({q4: "2"})} />
+                                <Radio name="q4" value="2" checked={this.state.q4 === "2"} onChange={()=>this.setState({q4: "2"})} />
                                 <label>投資していない</label>
                                 <br />
-                                <input type="radio" name="q4" value="0" checked={this.state.q4 === "0"} onChange={()=>this.setState({q4: "0"})} />
+                                <Radio name="q4" value="0" checked={this.state.q4 === "0"} onChange={()=>this.setState({q4: "0"})} />
                                 <label>投資している or 投資を検討している</label>
                             </dd>
                             <dt>Q5:プレゼント用口座を持っていますか？</dt>
                             <dd>
-                                <input type="radio" name="q5" value="2" checked={this.state.q5 === "2"} onChange={()=>this.setState({q5: "2"})} />
+                                <Radio name="q5" value="2" checked={this.state.q5 === "2"} onChange={()=>this.setState({q5: "2"})} />
                                 <label>持っている</label>
                                 <br />
-                                <input type="radio" name="q5" value="0" checked={this.state.q5 === "0"} onChange={()=>this.setState({q5: "0"})} />
+                                <Radio name="q5" value="0" checked={this.state.q5 === "0"} onChange={()=>this.setState({q5: "0"})} />
                                 <label>持っていない</label>
                             </dd>
                         </dl>
