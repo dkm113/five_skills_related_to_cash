754d7c586469dacd8405ec4793fde0b2e7344408
dkm113 <gkojima1221@gmail.com>
2020-11-23 14:14:15 +0900

    [update]: Material-UIの追加。Reactバージョンを16に引き下げ。

--- src/components/start.js
+++ src/components/start.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../index.css';
 
 class StartApp extends Component {
 
@@ -9,8 +10,13 @@ class StartApp extends Component {
     render() {
         return (
             <div>
-                <h2>さあ、はじまるよ</h2>
-                <button onClick={this.startForm.bind(this)}>5つの力を診断-さあ、あなたの5つの力は？-</button>
+                <div className="container">
+                    <div className="startVisual">
+                        <img width="600px" src="https://cdn.shortpixel.ai/client/to_avif,q_glossy,ret_img,w_960/https://liberaluni.com/wp-content/uploads/2018/04/191003itutunotikara1.png" />
+                        <br />
+                        <button onClick={this.startForm.bind(this)}>5つの力を診断-さあ、あなたの5つの力は？-</button>
+                    </div>
+                </div>
             </div>
         )
     }
